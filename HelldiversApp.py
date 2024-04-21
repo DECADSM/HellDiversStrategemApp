01794c451d092bd208d873b0db0ae34cf4d805b4
decadsm <71032757+DECADSM@users.noreply.github.com>
2024-04-20 21:59:13 -0400

    trying to build apk

--- HelldiversApp.py
+++ HelldiversApp.py
@@ -1,10 +1,9 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import Screen
 
-
-class Strategem(Widget):
+class Strategem(Screen):
     pass
-
 
 class Helldiver(App):
     def build(self):
