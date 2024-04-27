da957501d73d518d1799724492b1ae144a0413cc
decadsm <71032757+DECADSM@users.noreply.github.com>
2024-04-27 10:27:34 -0400

    Up button prints on press

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen
 
 class Strategem(Screen):
+    def pressed(self):
+        print('Fuck you')
     pass
 
 class Helldiver(App):
     def build(self):
         return Strategem()
+    
 
 
 if __name__ == '__main__':
